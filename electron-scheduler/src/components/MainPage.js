d7696a3b8f427669193b73f89a49632d0ed5eaa3
KantKontrol <nick.derissio.tech@gmail.com>
2020-10-19 22:15:05 -0400

    fixed useState and methods causing issues

--- electron-scheduler/src/components/MainPage.js
+++ electron-scheduler/src/components/MainPage.js
@@ -7,35 +7,33 @@ export default function MainPage(){
 
     const [pageState, setPageState] = useState({
         showModal: false,
-        appointments: [],
-        setModal: function(value){
-            setPageState({ ...pageState, showModal: value });
-        },
-        createAppointment: function(data){ //add appointments to appointment array here
-
-            let app = pageState.appointments; //pull from state
-
-            console.log(app)
-
-            app.push(data); //modify pulled data
-            
-            setPageState({ ...pageState, appointments: app, showModal: false }); //update state
-
-          /*  JSONhandler.writeData(app, (err) => {
-                if(err)
-                    console.log("error writing data");
-                else
-                  console.log("success");
-            });*/
-        }
+        appointments: []
     });
+
+    const setModal = function(value){
+        setPageState({ ...pageState, showModal: value });
+    }
+
+    const createAppointment = function(data){
+        let app = pageState.appointments; //pull from state
+
+        console.log(app)
+
+        app.push(data); //modify pulled data
+        
+        setPageState({ ...pageState, appointments: app, showModal: false }); //update state
+        
+        //update DB
+        accessDB("appDB", "apps", "put", data);
+
+    }
 
 
     useEffect(() => {
         accessDB("appDB", "apps", "get").then(data => {
+            console.log(data)
             setPageState({ ...pageState, appointments: data});
         });
-        
     }, []);
 
     console.log(pageState.appointments)
@@ -49,13 +47,13 @@ export default function MainPage(){
                 </div>
 
                 <div className="row">
-                    <button className="btn btn-primary d-block mx-auto mt-1" onClick={() => pageState.setModal(true)}>Create Appointment</button>
+                    <button className="btn btn-primary d-block mx-auto mt-1" onClick={() => setModal(true)}>Create Appointment</button>
                 </div>
 
                 <ALoader appointments={pageState.appointments} />
             
             </div>
-            <InputModal showModal={pageState.showModal} setModal={pageState.setModal} createAppointment={pageState.createAppointment}/>
+            <InputModal showModal={pageState.showModal} setModal={setModal} createAppointment={createAppointment}/>
         </>
     );
     
