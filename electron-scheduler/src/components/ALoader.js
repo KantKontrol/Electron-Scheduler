61b731e6fd4631df88f332747c216c5ccf92af8b
KantKontrol <nick.derissio.tech@gmail.com>
2020-10-20 21:45:20 -0400

    created an implemented time and date sort functions

--- electron-scheduler/src/components/ALoader.js
+++ electron-scheduler/src/components/ALoader.js
@@ -4,11 +4,50 @@ import SortBar from "./SortBar";
 
 export default function ALoader(props){
 
-    const [sortDateDirection, setSortDateDirection] = useState("ascend");
-
     let { appointments, removeApp } = props;
 
+    const [sortDateDirection, setSortDateDirection] = useState(0);
+    const [sortTimeDirection, setSortTimeDirection] = useState(0);
+
+    
+
     //add time sort here
+    const timeComparatorAscending = (a, b) => {
+
+        if(a.ampm === "AM" && b.ampm === "PM")
+            return -1;
+        else if(a.ampm === "PM" && b.ampm === "AM")
+            return 1;
+        else if(a.hour === b.hour){
+            if(a.minute < b.minute)
+                return -1;
+        }
+        else if(a.hour < b.hour)
+            return -1;
+        else if(a.hour > b.hour)
+            return 1;
+        else
+            return 0;
+    }
+
+    const timeComparatorDecending = (a,b) => {
+        console.log({a,b})
+
+        if(a.ampm === "AM" && b.ampm === "PM")
+            return 1;
+        else if(a.ampm === "PM" && b.ampm === "AM")
+            return -1;
+        else if(a.hour === b.hour){
+            if(a.minute > b.minute)
+                return -1;
+        }
+        else if(a.hour > b.hour)
+            return -1;
+        else if(a.hour < b.hour)
+            return 1;
+        else
+            return 0;
+    }
 
     const dateComparatorAscending = (a, b) => {
         if(a.date < b.date)
@@ -27,29 +66,34 @@ export default function ALoader(props){
         else
             return 0;
     }
-    
-    const runSortDate = () => {
-        appointments.sort(dateComparatorAscending);
-        console.log(appointments);
-    }
 
     useEffect(() => { //if sortDate direction changes
-        
-        if(sortDateDirection === "ascend")
+
+        if(sortDateDirection === 0)
             appointments.sort(dateComparatorAscending);
         else
             appointments.sort(dateComparatorDecending);
 
     }, [sortDateDirection]);
 
-    const changeDateDirection = () => sortDateDirection === "ascend" ? setSortDateDirection("descend") : setSortDateDirection("ascend");
+    useEffect(() => { //if sortDate direction changes
+
+        if(sortTimeDirection === 0)
+            appointments.sort(timeComparatorAscending);
+        else
+            appointments.sort(timeComparatorDecending);
+
+    }, [sortTimeDirection]);
+
+    const changeDateDirection = () => sortDateDirection === 0 ? setSortDateDirection(1) : setSortDateDirection(0);
+    const changeTimeDirection = () => sortTimeDirection === 0 ? setSortTimeDirection(1) : setSortTimeDirection(0);
     
 
     return (
         <div className="row">
             <div className="col-12">
                 <table className="table adisplay-main table-striped" cellPadding={0} cellSpacing={0}>
-                    <SortBar changeDateDirection={changeDateDirection} sortDateDirection={sortDateDirection} />
+                    <SortBar changeDateDirection={changeDateDirection} sortDateDirection={sortDateDirection} changeTimeDirection={changeTimeDirection} sortTimeDirection={sortTimeDirection} />
                     <tbody>
                         {
                             appointments.length > 0 ? appointments.map(e => {
--- electron-scheduler/src/components/SortBar.js
+++ electron-scheduler/src/components/SortBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SortBar(props){
 
-    let {sortDateDirection, changeDateDirection, sortTimeDirection} = props;
+    let {sortDateDirection, changeDateDirection, sortTimeDirection, changeTimeDirection} = props;
 
     return (
         <thead>
@@ -10,8 +10,8 @@ export default function SortBar(props){
                 <th scope="col">#</th>
                 <th scope="col">Parent Name</th>
                 <th scope="col">Child Name</th>
-                <th scope="col">Date <div className={sortDateDirection == "ascend" ? "sortImageUp" : "sortImageDown"} onClick={() => changeDateDirection()}><img src="/assets/img/arrow.png" alt="arrow"></img></div></th>
-                <th scope="col">Time <div className={sortTimeDirection == "ascend" ? "sortImageUp" : "sortImageDown"}><img src="/assets/img/arrow.png" alt="arrow"></img></div></th>
+                <th scope="col">Date <div onClick={() => changeDateDirection()} style={{ display: "inline", maxWidth: "30px"}}><img className={sortDateDirection === 0 ? "sortImageUp" : "sortImageDown"} src="/assets/img/arrow.png" alt="arrow"></img></div></th>
+                <th scope="col">Time <div onClick={() => changeTimeDirection()} style={{ display: "inline", maxWidth: "30px"}}><img className={sortTimeDirection == 0 ? "sortImageUp" : "sortImageDown"} src="/assets/img/arrow.png" alt="arrow"></img></div></th>
                 <th scope="col">Delete</th>
             </tr>
         </thead>
