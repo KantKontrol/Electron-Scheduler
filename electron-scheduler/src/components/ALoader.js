8727d5e5b89d72c4c9b865a2c17001273ab20015
KantKontrol <nick.derissio.tech@gmail.com>
2020-10-20 21:26:11 -0400

    created comparator functions for date

--- electron-scheduler/src/components/ALoader.js
+++ electron-scheduler/src/components/ALoader.js
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ADisplay from "./ADisplay";
 import SortBar from "./SortBar";
 
 export default function ALoader(props){
 
+    const [sortDateDirection, setSortDateDirection] = useState("ascend");
+
     let { appointments, removeApp } = props;
 
     //add time sort here
+
+    const dateComparatorAscending = (a, b) => {
+        if(a.date < b.date)
+            return -1;
+        else if(a.date > b.date)
+            return 1;
+        else
+            return 0;
+    }
+
+    const dateComparatorDecending = (a,b) => {
+        if(a.date > b.date)
+            return -1;
+        else if(a.date < b.date)
+            return 1;
+        else
+            return 0;
+    }
+    
+    const runSortDate = () => {
+        appointments.sort(dateComparatorAscending);
+        console.log(appointments);
+    }
+
+    useEffect(() => { //if sortDate direction changes
+        
+        if(sortDateDirection === "ascend")
+            appointments.sort(dateComparatorAscending);
+        else
+            appointments.sort(dateComparatorDecending);
+
+    }, [sortDateDirection]);
+
+    const changeDateDirection = () => sortDateDirection === "ascend" ? setSortDateDirection("descend") : setSortDateDirection("ascend");
+    
 
     return (
         <div className="row">
             <div className="col-12">
                 <table className="table adisplay-main table-striped" cellPadding={0} cellSpacing={0}>
-                    <SortBar />
+                    <SortBar changeDateDirection={changeDateDirection} sortDateDirection={sortDateDirection} />
                     <tbody>
                         {
                             appointments.length > 0 ? appointments.map(e => {
--- electron-scheduler/src/components/SortBar.js
+++ electron-scheduler/src/components/SortBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SortBar(props){
 
-    let {sortDirection} = props;
+    let {sortDateDirection, changeDateDirection, sortTimeDirection} = props;
 
     return (
         <thead>
@@ -10,8 +10,8 @@ export default function SortBar(props){
                 <th scope="col">#</th>
                 <th scope="col">Parent Name</th>
                 <th scope="col">Child Name</th>
-                <th scope="col">Date</th>
-                <th scope="col">Time <div className={sortDirection == "ascend" ? "sortImageUp" : "sortImageDown"}><img src="/assets/img/arrow.png" alt="arrow"></img></div></th>
+                <th scope="col">Date <div className={sortDateDirection == "ascend" ? "sortImageUp" : "sortImageDown"} onClick={() => changeDateDirection()}><img src="/assets/img/arrow.png" alt="arrow"></img></div></th>
+                <th scope="col">Time <div className={sortTimeDirection == "ascend" ? "sortImageUp" : "sortImageDown"}><img src="/assets/img/arrow.png" alt="arrow"></img></div></th>
                 <th scope="col">Delete</th>
             </tr>
         </thead>
